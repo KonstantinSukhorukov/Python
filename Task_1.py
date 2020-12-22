dd65fc39f0f30723a9b67586a057e555f252c7f8
Konstantin <ovs29@list.ru>
2020-12-23 01:50:42 +0300

    Homework 1 Task 1

---
+++ Task_1.py
@@ -0,0 +1,40 @@
+'''
+Поработайте с переменными, создайте несколько, выведите на экран,
+запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+'''
+
+a = 13
+b= 6.7
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+print(a > b)
+print(a == b)
+print(a < b)
+print(a != b)
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(b / a)
+print(a ** b)
+print(b ** a)
+print(a ** (1 / b))
+print(b ** (1 / a))
+print(a ** (- b))
+print(b ** (- a))
+print(a // b)
+print(b // a)
+print(a % b)
+print(b % a)
+
+c = (a > b)
+d = (a == b)
+print(c)
+print(type(c))
+print(d)
+print(type(d))
