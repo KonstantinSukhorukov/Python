4ef2208568b5ab6e85eda3197370994af667606e
Konstantin <ovs29@list.ru>
2021-01-22 18:33:33 +0300

    Коммичу задачи 1, 2, 3 из ДЗ№7

--- HW5_Task_2.py
+++
@@ -1,9 +0,0 @@
-# Создать текстовый файл (не программно), сохранить в нем несколько строк, выполнить
-# подсчет количества строк, количества слов в каждой строке.
-
-
-with open('text.txt') as f:
-    lines = f.readlines()
-print('Количество строк: ', len(lines))
-for num_line, line in enumerate(lines, start=1):
-    print('{} строка содержит {} слов'.format(num_line, len(line.split())))
